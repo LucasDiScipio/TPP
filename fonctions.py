921fc38eb64a55219c44de5e9a2a163b93454471
LucasDiScipio <lucasdiscipio@hotmail.com>
2021-10-14 16:02:37 +0200

    ajout des indicateurs (numerique, kanji, ...)

--- fonctions.py
+++ fonctions.py
@@ -1,4 +1,14 @@
 def string_to_binary(chaine):
+    """ Encode les caracteres de la chaine en mots de 8 bits
+    
+    INPUT
+    -----
+    - chaine: une chaine de caracteres
+    
+    OUTPUT
+    ------
+    - encodage : liste contenant les mots de 8 bits qui encodent les caracteres
+    """
     
     # liste des caracteres de la chaine encodes en binaire
     encodage = []
@@ -13,3 +23,25 @@ def string_to_binary(chaine):
             encodage[-1] = '0' + encodage[-1]
     
     return encodage
+
+
+def add_mode_indicator(liste, mode):
+    """ Ajoute le mode d'encodage au debut de liste
+    INPUTS
+    ------
+    - liste :
+    - mode :
+
+    Return
+    ------
+    - liste :
+    """
+    
+    mode_indicators = {'numeric': '0001', 
+                       'alphanumeric': '0010',
+                       'byte': '0100',
+                       'kanji': '1000'}
+    
+    liste = [mode_indicators[mode]] + liste
+
+    return liste
--- main.py
+++ main.py
@@ -1,6 +1,1 @@
 from fonctions import *
-
-
-
-
-
