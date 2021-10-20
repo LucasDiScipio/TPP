e340c8bffe03044159407b9db1f5b6087da40f93
LucasDiScipio <lucasdiscipio@hotmail.com>
2021-10-20 16:22:51 +0200

    test

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from bitarray import *
 from fonctions import *
+
+chaine = "test"
+mode = "byte"
+version = 1
+liste = string_to_binary(chaine)
+
+print(liste)
+
+liste = add_mode_indicator(liste, mode)
+
+print(liste)
+
+liste = add_character_count_indicator(chaine, liste, mode, version)
+
+print(liste)
