479e96508d993caf2b14f8de58477ee767ececa0
LucasDiScipio <lucas.discipio@student.unamur.be>
2021-11-03 12:36:48 +0100

    debut d'implementation des polynomes generateurs

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fonctions import *
+from numpy.polynomial import Polynomial
+import pandas as pd
 
 chaine = "oui"
 mode = "byte"
 version = 1
 EC_lvl = 'Q'
 
-data_codewords = data_encoding(chaine, mode, version, EC_lvl)
+# degre du polynome generateur
+df_Error_correction_table = pd.read_csv("./data/Error Correction Table.csv", index_col="Version and EC Level")
+version_and_EC_lvl = str(version) + '-' + EC_lvl
+degree = int(df_Error_correction_table.loc[version_and_EC_lvl, 'EC Codewords Per Block'] - 1)
 
-print(data_codewords)
+
+
+
+print(p.coef)
+
 
 
 
