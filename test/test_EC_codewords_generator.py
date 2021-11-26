394f67e9dd5f1b854b375f8e24be37fcd2b1aff1
LucasDiScipio <lucas.discipio@student.unamur.be>
2021-11-26 12:37:37 +0100

    suite testing :)

---
+++ test/test_EC_codewords_generator.py
@@ -0,0 +1,4 @@
+from fonctions import EC_codewords_generator
+
+message_polynomial = np.array([32,91,11,120,209,114,220,77,67,64,236,17,236])
+generator_polynomial = np.array([0,74,152,176,100,86,100,106,104,130,218,206,140,78]) 
--- test/test_add_mode_indicator.py
+++ test/test_add_mode_indicator.py
@@ -1,21 +1,19 @@
-from fonctions import *
+from fonctions import add_mode_indicator
+from bitarray import *
 
 # add_mode_indicator(liste, mode)
 def test_add_mode_indicator_return_numeric():
-    output = add_mode_indicator([], mode='numeric')
+    output = add_mode_indicator(liste=[], mode='numeric')
     assert output[0] == bitarray('0001')
 
 def test_add_mode_indicator_return_alphanumeric():
-    output = add_mode_indicator([], mode='alphanumeric')
+    output = add_mode_indicator(liste=[], mode='alphanumeric')
     assert output[0] == bitarray('0010')
 
 def test_add_mode_indicator_return_byte():
-    output = add_mode_indicator([], mode='byte')
+    output = add_mode_indicator(liste=[], mode='byte')
     assert output[0] == bitarray('0100')
 
 def test_add_mode_indicator_return_kanji():
-    output = add_mode_indicator([], mode='kanji')
+    output = add_mode_indicator(liste=[], mode='kanji')
     assert output[0] == bitarray('1000')
-
-
-# add_character_count_indicator(chaine, liste, mode, version)
