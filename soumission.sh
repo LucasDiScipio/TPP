c8d7ea573122f03c8a7c35bc2180eec79353b91d
LucasDiScipio <lucasdiscipio@hotmail.com>
2021-12-15 16:25:34 +0100

    boucle sur les qr codes

--- soumission.sh
+++ soumission.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=test
-#SBATCH --array=1-10000
-#SBATCH --test=0
+#SBATCH --array=1-1000
 #SBATCH --time=00:01:00 # days-hh-mm-ss
 #SBATCH --mem-per-cpu=512 #megabytes
 
 ml Python
 ml matplotlib
 
-echo "Task_ID : $SLURM_ARRAY_TASK_ID"
-python main.py $SLURM_ARRAY_TASK_ID $TEST
+i=0
+while [ $i -le 10 ]
+do
+    echo "Task_ID : $SLURM_ARRAY_TASK_ID"
+    python main.py $SLURM_ARRAY_TASK_ID $i
+    i=$(( $i + 1 ))
+done
